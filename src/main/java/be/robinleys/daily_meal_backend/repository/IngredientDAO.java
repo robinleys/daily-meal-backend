c28bb1a66464d61cafccac462f332448044d8a87
Robin Leys <131713950+robinleys@users.noreply.github.com>
2024-12-02 14:27:09 +0100

    add IngredientDAO

---
+++ src/main/java/be/robinleys/daily_meal_backend/repository/IngredientDAO.java
@@ -0,0 +1,45 @@
+package be.robinleys.daily_meal_backend.repository;
+
+import be.robinleys.daily_meal_backend.model.Ingredient;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class IngredientDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public IngredientDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void create(Ingredient ingredient) {
+        var sql = "INSERT INTO ingredients (name, stock, unit) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, ingredient.getName(), ingredient.getStock(), ingredient.getUnit());
+    }
+
+    public List<Ingredient> findAll() {
+        var sql = "SELECT * FROM ingredients";
+        var bprm = new BeanPropertyRowMapper<>(Ingredient.class);
+        return jdbcTemplate.query(sql, bprm);
+    }
+
+    public Ingredient findById(int id) {
+        var sql = "SELECT * FROM ingredients WHERE id = ?";
+        var bprm = new BeanPropertyRowMapper<>(Ingredient.class);
+        return jdbcTemplate.queryForObject(sql, bprm, id);
+    }
+
+    public void update(Ingredient ingredient) {
+        var sql = "UPDATE ingredients SET name = ?, stock = ?, unit = ? WHERE id = ?";
+        jdbcTemplate.update(sql, ingredient.getName(), ingredient.getStock(), ingredient.getUnit(), ingredient.getId());
+    }
+
+    public void delete(int id) {
+        var sql = "DELETE FROM ingredients WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}
