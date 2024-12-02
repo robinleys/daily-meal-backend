48ade5773eaa9c06834934c780eed277b3f9b1fa
Robin Leys <131713950+robinleys@users.noreply.github.com>
2024-12-02 14:27:32 +0100

    add IngredientController

---
+++ src/main/java/be/robinleys/daily_meal_backend/controller/IngredientController.java
@@ -0,0 +1,44 @@
+package be.robinleys.daily_meal_backend.controller;
+
+import be.robinleys.daily_meal_backend.model.Ingredient;
+import be.robinleys.daily_meal_backend.repository.IngredientDAO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/ingredients")
+public class IngredientController {
+
+    private final IngredientDAO ingredientDAO;
+
+    public IngredientController(IngredientDAO ingredientDAO) {
+        this.ingredientDAO = ingredientDAO;
+    }
+
+    @GetMapping
+    public List<Ingredient> getIngredients() {
+        return ingredientDAO.findAll();
+    }
+
+    @GetMapping("/{id")
+    public Ingredient getIngredientById(@RequestParam int id) {
+        return ingredientDAO.findById(id);
+    }
+
+    @PostMapping
+    public void addIngredient(@RequestBody Ingredient ingredient) {
+        ingredientDAO.create(ingredient);
+    }
+
+    @PutMapping
+    public void updateIngredient(@RequestBody Ingredient ingredient) {
+        ingredientDAO.update(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteIngredient(@PathVariable int id) {
+        ingredientDAO.delete(id);
+    }
+}
