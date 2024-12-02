bd8948db5cb5688ee2a4d8b0e665b268a39bcf67
Robin Leys <131713950+robinleys@users.noreply.github.com>
2024-12-02 09:15:35 +0100

    first commit

---
+++ src/main/java/be/robinleys/daily_meal_backend/DailyMealBackendApplication.java
@@ -0,0 +1,13 @@
+package be.robinleys.daily_meal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMealBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyMealBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/robinleys/daily_meal_backend/DailyMealBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.robinleys.daily_meal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyMealBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
