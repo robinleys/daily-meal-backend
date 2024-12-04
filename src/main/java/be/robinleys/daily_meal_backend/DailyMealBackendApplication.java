1fe56bd982b863c3b40438a2a8f9acbf12e90c71
Robin Leys <131713950+robinleys@users.noreply.github.com>
2024-12-04 14:51:47 +0100

    add testing

--- src/main/java/be/robinleys/daily_meal_backend/DailyMealBackendApplication.java
+++ src/main/java/be/robinleys/daily_meal_backend/DailyMealBackendApplication.java
@@ -1,10 +1,30 @@
 package be.robinleys.daily_meal_backend;
 
+import be.robinleys.daily_meal_backend.repository.IngredientDAO;
+import be.robinleys.daily_meal_backend.repository.RecipeDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DailyMealBackendApplication {
+public class DailyMealBackendApplication implements CommandLineRunner {
+    private final RecipeDAO recipeDAO;
+    private final IngredientDAO ingredientDAO;
+
+    public DailyMealBackendApplication(RecipeDAO recipeDAO, IngredientDAO ingredientDAO) {
+        this.recipeDAO = recipeDAO;
+        this.ingredientDAO = ingredientDAO;
+    }
+
+    public void run(String... args) throws Exception {
+        System.out.println(recipeDAO.findAll());
+        System.out.println(ingredientDAO.findAll());
+//        recipeDAO.addIngredientToRecipe(1,1,1);
+//        recipeDAO.addIngredientToRecipe(1,2,200);
+//        recipeDAO.addIngredientToRecipe(1,3,400);
+
+        System.out.println(recipeDAO.getIngredientsForRecipe(1));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DailyMealBackendApplication.class, args);
