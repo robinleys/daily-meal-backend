df08f4c5f0bccad3c8148512eab65152ca62dc14
Robin Leys <131713950+robinleys@users.noreply.github.com>
2024-12-05 19:40:50 +0100

    format sql

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,20 +1,23 @@
-CREATE TABLE IF NOT EXISTS ingredients (
-                                       id SERIAL PRIMARY KEY,
-                                       name VARCHAR(255) NOT NULL,
-                                       stock numeric(10,2) NOT NULL,
-                                       unit VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS ingredients
+(
+    id    SERIAL PRIMARY KEY,
+    name  VARCHAR(255)   NOT NULL,
+    stock NUMERIC(10, 2) NOT NULL,
+    unit  VARCHAR(255)   NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS recipes (
-                                       id SERIAL PRIMARY KEY,
-                                       name VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS recipes
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS recipe_ingredients (
-                                                  recipe_id INT NOT NULL,
-                                                  ingredient_id INT NOT NULL,
-                                                  quantity NUMERIC(10,2) NOT NULL,
-                                                  PRIMARY KEY (recipe_id, ingredient_id),
-                                                  FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
-                                                  FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS recipe_ingredients
+(
+    recipe_id     INT            NOT NULL,
+    ingredient_id INT            NOT NULL,
+    quantity      NUMERIC(10, 2) NOT NULL,
+    PRIMARY KEY (recipe_id, ingredient_id),
+    FOREIGN KEY (recipe_id) REFERENCES recipes (id) ON DELETE CASCADE,
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients (id) ON DELETE CASCADE
 );
