5e85a6d4b31eb34238163fcab9672a7639ffcc13
Robin Leys <131713950+robinleys@users.noreply.github.com>
2024-12-05 19:42:26 +0100

    change tests in run method

--- src/main/java/be/robinleys/daily_meal_backend/DailyMealBackendApplication.java
+++ src/main/java/be/robinleys/daily_meal_backend/DailyMealBackendApplication.java
@@ -1,10 +1,14 @@
 package be.robinleys.daily_meal_backend;
 
+import be.robinleys.daily_meal_backend.model.Ingredient;
+import be.robinleys.daily_meal_backend.model.Recipe;
 import be.robinleys.daily_meal_backend.repository.IngredientDAO;
 import be.robinleys.daily_meal_backend.repository.RecipeDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DailyMealBackendApplication implements CommandLineRunner {
@@ -17,13 +21,25 @@ public class DailyMealBackendApplication implements CommandLineRunner {
     }
 
     public void run(String... args) throws Exception {
-        System.out.println(recipeDAO.findAll());
-        System.out.println(ingredientDAO.findAll());
+//        ingredientDAO.create(new Ingredient("Chicken", 1, "unit"));
+//        ingredientDAO.create(new Ingredient("Beef", 3, "unit"));
+//        ingredientDAO.create(new Ingredient("Noodles", 2, "unit"));
+//        ingredientDAO.create(new Ingredient("Paprika", 5, "unit"));
+//        recipeDAO.create(new Recipe("Chicken paprika and noodles"));
+
+        List<Ingredient> ingredients = ingredientDAO.findAll();
+        for (Ingredient ingredient : ingredients) {
+            System.out.println(ingredient);
+        }
+
+
+
+
 //        recipeDAO.addIngredientToRecipe(1,1,1);
 //        recipeDAO.addIngredientToRecipe(1,2,200);
 //        recipeDAO.addIngredientToRecipe(1,3,400);
 
-        System.out.println(recipeDAO.getIngredientsForRecipe(1));
+//        System.out.println(recipeDAO.getIngredientsForRecipe(1));
     }
 
     public static void main(String[] args) {
